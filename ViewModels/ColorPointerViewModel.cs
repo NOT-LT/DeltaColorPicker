b8a9f609db6d7605cc3c2aa88e703a2797ea5310
Taha Aljamri <t.aljamri55@gmail.com>
2023-07-05 21:48:16 +0300

    Designing ColorPointUI, Worked on code-behind

--- ViewModels/ColorPointerViewModel.cs
+++ ViewModels/ColorPointerViewModel.cs
@@ -1,12 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using CommunityToolkit.Mvvm.ComponentModel;
+using ColorsPicker.Models;
+using System.Threading;
+using System.Windows.Threading;
+using System.Timers;
 
 namespace ColorsPicker.ViewModels
 {
-    class ColorPointerViewModel
+    class ColorPointerViewModel : ObservableObject, IDisposable
     {
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public ColorPointerViewModel()
+        {
+            timer.Interval = TimeSpan.FromSeconds(0.01);
+            timer.Tick += (s, e) => GetHoverColor();
+            timer.Start();
+        }
+
+        //private async void method1()
+        //{
+        //    while (true)
+        //    {
+        //        Dispatcher.CurrentDispatcher.InvokeAsync(() => GetHoverColor());
+        //    }
+
+        //}
+
+        private SolidColorBrush hoverColor;
+		public SolidColorBrush HoverColor
+        {
+			get { return hoverColor; }
+			set { hoverColor = value; OnPropertyChanged(nameof(HoverColor)); }
+        }
+
+        private SavedColor savedColor;
+        public SavedColor SavedColor
+        {
+            get { return savedColor; }
+            set { savedColor = value; OnPropertyChanged(nameof(SavedColor)); }
+        }
+
+        private BitmapSource bitmapSource;
+        public BitmapSource BitmapSource
+        {
+            get { return bitmapSource; }
+            set { bitmapSource = value; OnPropertyChanged(nameof(BitmapSource)); }
+        }
+
+        private Graphics graphics;
+        private int captureSize;
+        private Bitmap bitmap;
+        private Image ScreenImage;
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+        public void GetHoverColor()
+		{
+            try
+            {
+                var w32Mouse = new Win32Point();
+                GetCursorPos(ref w32Mouse);
+                captureSize = 80;
+                bitmap = new Bitmap(captureSize, captureSize);
+                graphics = Graphics.FromImage(bitmap);
+                graphics.CopyFromScreen(
+                    new System.Drawing.Point(w32Mouse.X - captureSize / 2, w32Mouse.Y - captureSize / 2),
+                    new System.Drawing.Point(0, 0),
+                    new System.Drawing.Size(captureSize, captureSize));
+                var color = bitmap.GetPixel(captureSize / 2, captureSize / 2);
+
+                 BitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    bitmap.GetHbitmap(),
+                    IntPtr.Zero,
+                    System.Windows.Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+
+                HoverColor = new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B));
+            }
+            catch (Exception){}
+
+        }
+
+        public void Dispose()
+        {
+            if (graphics != null)
+            {
+                graphics.Dispose();
+                graphics = null;
+            }
+
+            if (bitmap != null)
+            {
+                bitmap.Dispose();
+                bitmap = null;
+            }
+
+            if (ScreenImage != null)
+            {
+                ScreenImage.Dispose();
+                ScreenImage = null;
+            }
+
+            if (timer != null)
+            {
+                timer.Stop();
+                timer.Tick -= (s, e) => GetHoverColor();
+                timer = null;
+            }
+        }
+
     }
 }
--- Views/ColorPointerView.xaml.cs
+++ Views/ColorPointerView.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ColorsPicker.Views
 {
@@ -20,9 +26,64 @@ namespace ColorsPicker.Views
     /// </summary>
     public partial class ColorPointerView : Page
     {
+        //private DispatcherTimer _timer;
+        //private Graphics graphics;
+        //private int captureSize;
+        //private Bitmap bitmap;
+
         public ColorPointerView()
         {
             InitializeComponent();
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = TimeSpan.FromMilliseconds(100);
+            //_timer.Tick += Timer_Tick;
+            //_timer.Start();
         }
+
+        //[DllImport("user32.dll")]
+        //[return: MarshalAs(UnmanagedType.Bool)]
+        //internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        //[StructLayout(LayoutKind.Sequential)]
+        //internal struct Win32Point
+        //{
+        //    public Int32 X;
+        //    public Int32 Y;
+        //};
+
+        //private void Timer_Tick(object? sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        var w32Mouse = new Win32Point();
+        //        GetCursorPos(ref w32Mouse);
+        //        captureSize = 80;
+        //        bitmap = new Bitmap(captureSize, captureSize);
+        //        graphics = Graphics.FromImage(bitmap);
+        //        graphics.CopyFromScreen(
+        //            new System.Drawing.Point(w32Mouse.X - captureSize / 2, w32Mouse.Y - captureSize / 2),
+        //            new System.Drawing.Point(0, 0),
+        //            new System.Drawing.Size(captureSize, captureSize));
+        //        var color = bitmap.GetPixel(captureSize / 2, captureSize / 2);
+
+        //        var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+        //            bitmap.GetHbitmap(),
+        //            IntPtr.Zero,
+        //            System.Windows.Int32Rect.Empty,
+        //            BitmapSizeOptions.FromEmptyOptions());
+
+        //        Image.Source = bitmapSource;
+        //        SolidColorBrush brush = new SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B));
+        //        rect.Fill = brush;
+        //    }
+        //    catch (Exception)
+        //    {
+                
+        //    }
+           
+        //}
+
+        
+
     }
 }
